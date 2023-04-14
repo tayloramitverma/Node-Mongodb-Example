7319f517f4aaed5228ca2ab55898a9221661dcca
Amit <amit-c@everledger.io>
2023-04-14 11:51:59 +0530

    updated routes

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use("web-api/admin", adminRoutes);
+app.use("web-api/shop", shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found!</h1>");
